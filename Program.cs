d2f82142d6af3d3b8ba8626cccc2cc2a368c67ae
eduardafurst <eduardafurst8@gmail.com>
2026-03-30 19:45:07 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+Console.Clear();
+Console.WriteLine("========================================");
+Console.WriteLine("   SISTEMA EXPERT: Módulo de Boas-Vindas");
+Console.WriteLine("========================================\n");
+
+Console.Write("Olá, Recruta! Qual é o seu nome de Desenvolvedor(a)? ");
+string nome = Console.ReadLine();
+Console.Write($"Prazer, {nome}! Em qual ano você começou a programar? ");
+string entradaAno = Console.ReadLine();
+
+int anoInicio = int.Parse(entradaAno);
+int anosDeJornada = DateTime.Now.Year - anoInicio;
+
+Console.WriteLine("\n----------------------------------------");
+Console.WriteLine($"STATUS DO PERFIL: {nome.ToUpper()}");
+Console.WriteLine($"TEMPO DE ESTRADA: {anosDeJornada} ano(s) de experiência.");
+Console.WriteLine("----------------------------------------");
+Console.WriteLine("\n[Missão Cumprida! Pressione qualquer tecla para encerrar]");
+Console.ReadKey();
